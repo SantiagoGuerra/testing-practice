23f4d4b4b5ce4863685e9997857755074e0b2080
Santiago Guerra <guerrasantiago19@gmail.com>
2020-05-06 15:10:23 -0500

    Create caesar cipher function ciphers each letter in the plaintext using a shift

---
+++ src/Calculator.js
@@ -0,0 +1,9 @@
+export default function Calculator() {
+  this.add = (a, b) => a + b;
+
+  this.substract = (a, b) => a - b;
+
+  this.divide = (a, b) => a / b;
+
+  this.multiply = (a, b) => a * b;
+}
